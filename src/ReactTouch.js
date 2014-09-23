90415882bd1d62a2cf5facbb49db65f7f3fa068d
Dustan Kasten <dustan.kasten@gmail.com>
2014-09-23 14:18:42 -0400

    initializeTouchEvents before requiring TapEventPlugin

--- src/ReactTouch.js
+++ src/ReactTouch.js
@@ -4,14 +4,14 @@ var Router = require('./routing/Router');
 
 var ReactTouch = {
   start: function(componentClass, domNode, routes, useHistory) {
+    React.initializeTouchEvents(true);
+
     var EventPluginHub = require('react/lib/EventPluginHub');
     var TapEventPlugin = require('./thirdparty/TapEventPlugin');
 
     EventPluginHub.injection.injectEventPluginsByName({
       TapEventPlugin: TapEventPlugin
     });
-
-    React.initializeTouchEvents(true);
 
     Router.start(componentClass, domNode, routes, useHistory);
   }
